eab9a2d4739278445c66a9abe15717aa929e517b
Robin Frischmann <robin@rofrischmann.de>
2017-05-19 17:17:53 +0200

    update on plugin

--- modules/plugins/noVendorPrefix.js
+++ modules/plugins/noVendorPrefix.js
@@ -13,74 +13,35 @@ export function noVendorPrefix({ style, addWarning }: PluginInterface): void {
   objectEach(style, (value, property) => {
     const camelCasedProperty = camelCaseProperty(property)
 
+    let newProperty = property
+    const newValue = value
+
     if (isPrefixedProperty(camelCasedProperty)) {
       const unprefixedProperty = unprefixProperty(camelCasedProperty)
 
-      addWarning({
-        type: 'NO_VENDOR_PREFIX',
-        description: `The property "${property}" should not be vendor prefixed. Use "${unprefixedProperty}" instead.`
-        suggestion: unprefixedProperty,
-        property,
-        value
-      })
-    }
-
-    if (typeof value === 'string' && isPrefixedValue(value)) {
-      const unprefixedValue = unprefixValue(value)
-
-      warnings.push({
-        type: this.getType(),
-        hint: `The value "${value}" shoul not be vendor prefixes. Use "${suggestion}" instead.`
-        suggestion: unprefixedValue,
-        property,
-        value
-      })
-    }
-
-  })
-}
-
-export default () => noShorthand
-
-
-export default {
-  getType() {
-    return 'NO_VENDOR_PREFIX'
-  },
-  getPropertyHint(property, suggestion) {
-    return `Property "${property}" does not need vendor prefixes. Use "${suggestion}" instead.`
-  },
-  getValueHint(value, suggestion) {
-    return `Value "${value}" does not need vendor prefixes. Use "${suggestion}" instead.`
-  },
-  run(style, meta, warnings) {
-    for (const property in style) {
-      const camelCasedProperty = dashToCamelCase(property)
-      const value = style[property]
-
-      if (isPrefixedProperty(camelCasedProperty)) {
-        const unprefixedProperty = unprefixProperty(camelCasedProperty)
-
-        warnings.push({
-          type: this.getType(),
-          hint: this.getPropertyHint(property, unprefixedProperty),
+      if (autoFix) {
+        newProperty = unprefixedProperty
+      } else {
+        addWarning({
+          type: 'NO_VENDOR_PREFIX',
+          description: `The property "${property}" should not be vendor prefixed. Use "${unprefixedProperty}" instead.`,
           suggestion: unprefixedProperty,
           property,
           value
         })
       }
-
-      if (typeof value === 'string' && isPrefixedValue(value)) {
-        const unprefixedValue = unprefixValue(value)
-
-        warnings.push({
-          type: this.getType(),
-          hint: this.getValueHint(value, unprefixedValue),
-          suggestion: unprefixedValue,
-          property,
-          value
-        })
-      }
     }
-  }
+
+    if (typeof value === 'string' && isPrefixedValue(value)) {
+      const unprefixedValue = unprefixValue(value)
+
+      addWarning({
+        type: 'NO_VENDOR_PREFIX',
+        hint: `The value "${value}" shoul not be vendor prefixes. Use "${unprefixedValue}" instead.`,
+        suggestion: unprefixedValue,
+        property,
+        value
+      })
+    }
+  })
 }
